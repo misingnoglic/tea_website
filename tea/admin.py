a2fbb58de74d4ec8326cf1c47dec167e78a080e2
Arya Boudaie <aboudaie@brandeis.edu>
2017-06-18 13:24:29 -0500

    Added inlines to admin

--- tea/admin.py
+++ tea/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Tea, TeaType, Brand, Ingredient, Picture, Rating
 
 # Register your models here.
-for m in [Tea, TeaType, Brand, Ingredient, Picture, Rating]:
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+
+class PictureInline(admin.TabularInline):
+    model = Picture
+
+class RatingInline(admin.TabularInline):
+    model = Rating
+
+@admin.register(Tea)
+class TeaAdmin(admin.ModelAdmin):
+    inlines = [PictureInline, RatingInline]
+
+for m in [TeaType, Brand, Ingredient, Picture, Rating]:
     admin.site.register(m)
